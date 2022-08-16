4725aa9293ce14e462410378502db1ddc6bf9879
Leonardo Narciso <LNarcisoOV@gmail.com>
2022-08-16 10:02:08 +0000

    Enable mongo repositoy.

--- src/main/java/com/mongo/MongoStudyApplication.java
+++ src/main/java/com/mongo/MongoStudyApplication.java
@@ -2,8 +2,10 @@ package com.mongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.mc.repository")
 public class MongoStudyApplication {
 
 	public static void main(String[] args) {
